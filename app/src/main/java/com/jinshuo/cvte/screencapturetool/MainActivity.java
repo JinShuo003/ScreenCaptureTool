07340ffb18722296b8e3cd6429a17c2531d35f7e
--global <--global>
2023-12-05 18:10:42 +0800

    feat: 基于MediaProjection+MediaCodec实现了录屏视频流的编码，基于MediaCodec实现了preview部分的解码，TODO：将解码后的数据显示在surface上

--- app/src/main/java/com/jinshuo/cvte/screencapturetool/MainActivity.java
+++ app/src/main/java/com/jinshuo/cvte/screencapturetool/MainActivity.java
@@ -5,13 +5,18 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+import android.media.MediaCodec;
+import android.media.MediaFormat;
 import android.media.projection.MediaProjectionManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,40 +28,116 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Button btnStartCapture;
     private Button btnStopCapture;
+    private Button btnStartPreview;
     private Button btnScreenshot;
     private ImageView ivScreenshot;
-
+    private SurfaceView surfaceView;
     private boolean isScreenCaptureServiceConnected = false;
-    private boolean isScreenshotServiceConnected = false;
 
     private ScreenCaptureService.ScreenCaptureBinder screenCaptureBinder;
-    private ScreenshotService.ScreenshotBinder screenshotBinder;
 
+    private boolean isRecording = false;
     private MediaProjectionManager mediaProjectionManager;
+    private MediaCodec previewDecoder;
+    private MediaCodec saveVideoDecoder;
+
     private static final int SCREEN_CAPTURE_INTENT_REQUEST_CODE = 1000;
-    private static final int SCREENSHOT_INTENT_REQUEST_CODE = 1001;
 
     private static final int PERMISSION_REQUEST_CODE = 200;
 
     private DisplayMetrics displayMetrics;
+    private int displayWidth = 0;
+    private int displayHeight = 0;
+    private int displayDensity = 0;
 
     String[] permissions = new String[]{
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE};
 
+    ByteBuffer byteBuffer;
+    public static final int BUFFER_DATA_READY = 0;
+    private Handler mainHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message message) {
+            switch (message.what) {
+                case BUFFER_DATA_READY:
+                    byteBuffer = MainActivity.this.screenCaptureBinder.acquireByteBuffer();
+                    Message subThreadMessage = Message.obtain();
+                    subThreadMessage.what = BUFFER_DATA_READY;
+                    previewHandler.sendMessage(subThreadMessage);
+                    saveVideoHandler.sendMessage(subThreadMessage);
+            }
+            return false;
+        }
+    });
+    private Handler previewHandler;
+    private Handler saveVideoHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
         checkPermissions();
+        connectService();
     }
 
+    /**
+     * 初始化
+     */
+    private void init() {
+        btnStartCapture = findViewById(R.id.start_capture);
+        btnStopCapture = findViewById(R.id.end_capture);
+        btnStartPreview = findViewById(R.id.start_preview);
+        btnScreenshot = findViewById(R.id.screenshot);
+        ivScreenshot = findViewById(R.id.screenshot_image);
+        surfaceView = findViewById(R.id.surface);
+
+        mediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+        initDisplayInfo();
+
+        View.OnClickListener startCaptureOnClickListener = view -> {
+            startCaptureScreen();
+        };
+        View.OnClickListener endCaptureOnClickListener = view -> {
+            stopCaptureScreen();
+        };
+        View.OnClickListener startPreviewOnClickListener = view -> {
+            startPreview();
+        };
+        View.OnClickListener screenshotOnClickListener = view -> {
+            doScreenshot();
+        };
+
+        btnStartCapture.setOnClickListener(startCaptureOnClickListener);
+        btnStopCapture.setOnClickListener(endCaptureOnClickListener);
+        btnScreenshot.setOnClickListener(screenshotOnClickListener);
+    }
+
+    private void initDisplayInfo() {
+        displayMetrics = ScreenUtils.getScreenMetrics(this);
+        displayWidth = 720;
+        displayHeight = 1280;
+//        displayWidth = displayMetrics.widthPixels;
+//        displayHeight = displayMetrics.heightPixels;
+        displayDensity = displayMetrics.densityDpi;
+    }
+    /**
+     * 绑定到录屏服务
+     */
+    private void connectService() {
+        if (!isScreenCaptureServiceConnected) {
+            Intent intent = new Intent(this, ScreenCaptureService.class);
+            bindService(intent, screenCaptureServiceConnection, BIND_AUTO_CREATE);
+        }
+    }
     /**
      * 检查权限情况
      */
@@ -71,6 +152,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 请求权限的回调，如果未授权则拒绝服务
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
             grantResults) {
@@ -84,32 +168,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-    }
-
-    /**
-     * 初始化
-     */
-    private void init() {
-        btnStartCapture = findViewById(R.id.start_capture);
-        btnStopCapture = findViewById(R.id.end_capture);
-        btnScreenshot = findViewById(R.id.screenshot);
-        ivScreenshot = findViewById(R.id.screenshot_image);
-
-        mediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
-        displayMetrics = ScreenUtils.getScreenMetrics(this);
-
-        View.OnClickListener startCaptureOnClickListener = view -> {
-            startCaptureScreen();
-        };
-        View.OnClickListener endCaptureOnClickListener = view -> {
-            stopCaptureScreen();
-        };
-        View.OnClickListener screenshotOnClickListener = view -> {
-            doScreenshot();
-        };
-        btnStartCapture.setOnClickListener(startCaptureOnClickListener);
-        btnStopCapture.setOnClickListener(endCaptureOnClickListener);
-        btnScreenshot.setOnClickListener(screenshotOnClickListener);
     }
 
     /**
@@ -129,38 +187,81 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    /**
-     * 绑定截屏服务的connection对象
-     */
-    private ServiceConnection screenshotServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            isScreenshotServiceConnected = true;
-            screenshotBinder = (ScreenshotService.ScreenshotBinder) iBinder;
-            screenshotBinder.registerScreenInfo(displayMetrics);
+    private void startPreviewVideoDecoder() {
+        try {
+            previewDecoder = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
+        final MediaFormat format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, displayWidth, displayHeight);
+        format.setInteger(MediaFormat.KEY_BIT_RATE,  displayWidth * displayHeight);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
+//        //横屏
+//        byte[] header_sps = {0, 0, 0, 1, 103, 66, -128, 31, -38, 1, 64, 22, -24, 6, -48, -95, 53};
+//        byte[] header_pps = {0, 0 ,0, 1, 104, -50, 6, -30};
+        //竖屏
+        byte[] header_sps = {0, 0, 0, 1, 103, 66, -128, 31, -38, 2, -48, 40, 104, 6, -48, -95, 53};
+        byte[] header_pps = {0, 0 ,0, 1, 104, -50, 6, -30};
+//        format.setByteBuffer("csd-0", ByteBuffer.wrap(header_sps));
+//        format.setByteBuffer("csd-1", ByteBuffer.wrap(header_pps));
+        previewDecoder.configure(format, surfaceView.getHolder().getSurface(), null, 0);//mSurface对应需要展示surfaceview的surface
+        previewDecoder.start();
+    }
 
+    private Thread previewThread = new Thread(new Runnable() {
         @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            isScreenCaptureServiceConnected = false;
+        public void run() {
+            startPreviewVideoDecoder();
+            Looper.prepare();
+            previewHandler = new Handler(new Handler.Callback() {
+                @Override
+                public boolean handleMessage(@NonNull Message message) {
+                    switch (message.what) {
+                        case BUFFER_DATA_READY:
+                            return false;
+                        default:
+                            return false;
+                    }
+                }
+            });
+            Looper.loop();
         }
-    };
+    });
 
+    private Thread saveVideoThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            Looper.prepare();
+            saveVideoHandler = new Handler(new Handler.Callback() {
+                @Override
+                public boolean handleMessage(@NonNull Message message) {
+                    switch (message.what) {
+                        case BUFFER_DATA_READY:
+                            return false;
+                        default:
+                            return false;
+                    }
+                }
+            });
+            Looper.loop();
+        }
+    });
     /**
      * 开始录屏
      */
     private void startCaptureScreen() {
-        // 如果还未绑定服务，则先绑定
-        if (!isScreenCaptureServiceConnected) {
-            Intent intent = new Intent(this, ScreenCaptureService.class);
-            bindService(intent, screenCaptureServiceConnection, BIND_AUTO_CREATE);
-        }
-
         Intent screenCaptureIntent = mediaProjectionManager.createScreenCaptureIntent();
         startActivityForResult(screenCaptureIntent, SCREEN_CAPTURE_INTENT_REQUEST_CODE);
 
         btnStartCapture.setEnabled(false);
+        btnStartCapture.setEnabled(true);
         btnStopCapture.setEnabled(true);
+
+        // 启动预览解码器线程
+        previewThread.start();
+        isRecording = true;
         Toast.makeText(this, R.string.start_capture, Toast.LENGTH_SHORT).show();
     }
 
@@ -169,30 +270,35 @@ public class MainActivity extends AppCompatActivity {
      */
     private void stopCaptureScreen() {
         screenCaptureBinder.stopCapture();
+
         btnStartCapture.setEnabled(true);
         btnStopCapture.setEnabled(false);
+        btnStartCapture.setEnabled(false);
+
+        isRecording = false;
         Toast.makeText(this, R.string.stop_capture, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 开启预览
+     */
+    private void startPreview() {
+
     }
 
     /**
      * 截屏
      */
     private void doScreenshot() {
-        // 如果还未绑定服务，则先绑定
-        if (!isScreenshotServiceConnected) {
-            Intent intent = new Intent(this, ScreenshotService.class);
-            bindService(intent, screenshotServiceConnection, BIND_AUTO_CREATE);
+        if (!isRecording) {
+
+        } else {
+
         }
-        Intent screenCaptureIntent = mediaProjectionManager.createScreenCaptureIntent();
-        startActivityForResult(screenCaptureIntent, SCREENSHOT_INTENT_REQUEST_CODE);
-        Toast.makeText(this, R.string.screenshot, Toast.LENGTH_SHORT).show();
     }
 
     /**
-     * 接收用户授权结果
-     * @param requestCode
-     * @param resultCode
-     * @param data
+     * 接收授权结果
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -211,19 +317,9 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "User cancelled");
             }
         }
-        if(requestCode == SCREENSHOT_INTENT_REQUEST_CODE) {
-            if(resultCode == RESULT_OK) {
-                Bundle bundle = new Bundle();
-                bundle.putInt("code", resultCode);
-                bundle.putParcelable("data", data);
-                screenshotBinder.registerScreenCaptureRequestInfo(bundle);
-                screenshotBinder.prepareCaptureEnviroment();
-                Bitmap bitmap = screenshotBinder.screenshot();
-                ivScreenshot.setImageBitmap(bitmap);
-                Log.i(TAG, "screenshot");
-            } else {
-                Log.i(TAG, "User cancelled");
-            }
-        }
+    }
+
+    private void handleVideoStream() {
+
     }
 }
--- app/src/main/java/com/jinshuo/cvte/screencapturetool/ScreenCaptureService.java
+++ app/src/main/java/com/jinshuo/cvte/screencapturetool/ScreenCaptureService.java
@@ -9,7 +9,9 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
-import android.media.MediaRecorder;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.Binder;
@@ -18,20 +20,23 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Surface;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.ByteBuffer;
 
 public class ScreenCaptureService extends Service {
     private static final String TAG = "ScreenCaptureService";
 
     MediaProjection mediaProjection;
-    MediaRecorder mediaRecorder;
+    MediaCodec mediaCodec;
+    Surface surface;
     VirtualDisplay virtualDisplay;
+    ByteBuffer bufferData;
 
     boolean isRecording = false;
-    boolean isPausing = false;
+
+    boolean isRunning = true;
 
     int resultCode;
     Intent data;
@@ -49,6 +54,9 @@ public class ScreenCaptureService extends Service {
         Log.d(TAG, "screen capture service created");
     }
 
+    /**
+     * 前台服务通知
+     */
     private void createNotificationChannel() {
         Notification.Builder builder = new Notification.Builder(this.getApplicationContext()); //获取一个Notification构造器
         Intent nfIntent = new Intent(this, ScreenCaptureService.class); //点击后跳转的界面，可以设置跳转数据
@@ -87,67 +95,57 @@ public class ScreenCaptureService extends Service {
     }
 
     /**
-     * 获取MediaRecorder实例
-     * @return MediaRecorder instance
-     */
-    private MediaRecorder createMediaRecorder() {
-        String videoOutputDirectory = getExternalFilesDir("") + "/ScreenCapture";
-        StorageUtils.makeDirectory(videoOutputDirectory);
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-        Date curDate = new Date(System.currentTimeMillis());
-        String curTime = formatter.format(curDate).replace(" ", "");
-
-        Log.i(TAG, "Create MediaRecorder");
-        MediaRecorder mediaRecorder = new MediaRecorder();
-//        mediaRecorder.setPreviewDisplay(surface);
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);  //after setOutputFormat()
-        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);  //after setOutputFormat()
-        mediaRecorder.setVideoSize(mScreenWidth, mScreenHeight);  //after setVideoSource(), setOutFormat()
-        mediaRecorder.setVideoFrameRate(60);
-        mediaRecorder.setOutputFile(videoOutputDirectory + "/ScreenCapture_" + curTime + ".mp4");
-        int bitRate = 3 * mScreenWidth * mScreenHeight;
-        mediaRecorder.setVideoEncodingBitRate(3 * mScreenWidth * mScreenHeight);
-        Log.d(TAG, "createMediaRecorder: bigRate: " + bitRate);
-
-        try {
-            mediaRecorder.prepare();
-        } catch (IllegalStateException | IOException e) {
-            e.printStackTrace();
-        }
-
-        return mediaRecorder;
-    }
-
-    /**
-     * 获取VirtualDisplay实例
-     * @return VirtualDisplay instance
-     */
-    private VirtualDisplay createVirtualDisplay() {
-        return mediaProjection.createVirtualDisplay(TAG, mScreenWidth, mScreenHeight, mScreenDensity,
-                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, mediaRecorder.getSurface(), null, null);
-    }
-
-    /**
      * 开始录制屏幕
      */
     private void startCapture() {
-        mediaRecorder = createMediaRecorder();
-        virtualDisplay = createVirtualDisplay();
-        mediaRecorder.start();
+        startVideoEncoder();
         isRecording = true;
     }
+
+    private void startVideoEncoder() {
+        try {
+            mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        MediaFormat format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, mScreenWidth, mScreenHeight);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, mScreenWidth * mScreenHeight);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, 20);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
+        mediaCodec.configure(
+                format,
+                null,
+                null,
+                MediaCodec.CONFIGURE_FLAG_ENCODE);
+        surface = mediaCodec.createInputSurface();
+        virtualDisplay = mediaProjection.createVirtualDisplay(TAG, mScreenWidth, mScreenHeight, mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, surface, null, null);
+        mediaCodec.start();
+        acquireDataThread.start();
+    }
+
+    private Thread acquireDataThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            while (isRunning) {
+                int outputBufferId = mediaCodec.dequeueOutputBuffer(bufferInfo, 0);
+                if (outputBufferId >= 0) {
+                    bufferData = mediaCodec.getOutputBuffer(outputBufferId);
+                    mediaCodec.releaseOutputBuffer(outputBufferId, false);
+                }
+            }
+        }
+    });
 
     /**
      * 结束录制屏幕
      */
     public void stopCapture() {
         if (isRecording) {
-            mediaRecorder.stop();
-            mediaRecorder.reset();
+
         }
     }
 
@@ -158,12 +156,12 @@ public class ScreenCaptureService extends Service {
             virtualDisplay.release();
             virtualDisplay = null;
         }
-        if (mediaRecorder != null) {
+        if (mediaCodec != null) {
             if (isRecording) {
-                mediaRecorder.stop();
+                mediaCodec.stop();
             }
-            mediaRecorder.release();
-            mediaRecorder = null;
+            mediaCodec.release();
+            mediaCodec = null;
         }
         if (mediaProjection != null) {
             mediaProjection.stop();
@@ -197,6 +195,10 @@ public class ScreenCaptureService extends Service {
         public void prepareCaptureEnviroment() {
             createNotificationChannel();
             mediaProjection = createMediaProjection();
+        }
+
+        public ByteBuffer acquireByteBuffer() {
+            return bufferData;
         }
     }
     @Override
